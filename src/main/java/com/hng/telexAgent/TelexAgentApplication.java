16443199d36cd6aa6f99e36018f94ef2f6e41282
Adedayo-Data <theophilusdayo@outlook.com>
2025-11-02 16:27:10 +0100

    Initial Commit

---
+++ src/main/java/com/hng/telexAgent/TelexAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hng.telexAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelexAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelexAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hng/telexAgent/TelexAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng.telexAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelexAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
